fa30ec22ee49443a595c15d545c7d79c18c6f091
Sawyer Ruben <sawyer.ruben@gmail.com>
2020-05-12 14:00:29 -0400

    setting up import for robin_stocks

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,5 +1,20 @@
+from robin_stocks import *
+
+
+def login_to_account(username, password):
+    access_token = login(username, password)['access_token']
+    return bool(access_token)
+
+
 def main():
-    print("Hello, World!")
+    logged_in = login_to_account("sawyer.ruben@gmail.com", "Hotrod17!saw")
+    if logged_in:
+        stocks = build_holdings()
+        for k, v in stocks.items():
+            print(k, v)
+
+        order_buy_market("GIS", 1)
+
 
 
 if __name__ == "__main__":
